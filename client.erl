c03dbba446b53b97d5eef7bdfdcf64a2d179b9d0
Johan Jörtsö <johan.jortso@hiq.se>
2025-03-13 13:51:55 +0100

    Return server reply from client:start/0

--- client.erl
+++ client.erl
@@ -5,9 +5,13 @@ start() ->
     {ok, Socket} = gen_tcp:connect("localhost", 7777, [list, {packet, 0}]),
     io:format("Client: connected to: ~p~n", [Socket]),
     gen_tcp:send(Socket, "hello"),
-    receive
-        Reply ->
-            io:format("Client: reply: ~p~n", [Reply])
-    after timer:seconds(30) ->
-        io:format("Client: no data received.~n")
-    end.
+    ReplyFromServer =
+        receive
+            Reply ->
+                io:format("Client: reply: ~p~n", [Reply]),
+                Reply
+        after timer:seconds(30) ->
+            io:format("Client: no data received.~n"),
+            {}
+        end,
+    ReplyFromServer.
