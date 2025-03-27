c36f8b29305b728604f663a81f11cc71cc74e1f4
Johan Jörtsö <johan.jortso@gmail.com>
2025-03-27 11:14:29 +0100

    Add IP as parameter to client

--- client.erl
+++ client.erl
@@ -1,8 +1,13 @@
 -module(client).
 -compile([export_all, nowarn_export_all]).
 
-start(Port) ->
-    {ok, Socket} = gen_tcp:connect("localhost", Port, [list, {packet, 0}]),
+start(Host, Port) ->
+    %% Get IP address in Erlang tuple format.
+    %% That is -type ip4_address() :: {0..255, 0..255, 0..255, 0..255} or
+    %% -type ip6_address() :: {0..65535, 0..65535, 0..65535, 0..65535,
+    %%                         0..65535, 0..65535, 0..65535, 0..65535}.
+    {ok, Ip} = parse_ip(Host),
+    {ok, Socket} = gen_tcp:connect(Ip, Port, [list, {packet, 0}]),
     ok = io:format("Client: Connected to: ~p~n", [Socket]),
     ok = gen_tcp:send(Socket, "Hello"),
     ReplyFromServer =
@@ -17,3 +22,17 @@ start(Port) ->
     ok = io:format("Client: Closing connection.~n"),
     ok = gen_tcp:close(Socket),
     ReplyFromServer.
+
+-spec parse_ip(list() | inet:ip_address()) -> inet:ip_address().
+parse_ip(Ip = "localhost") ->
+    %% String "localhost" address is accepted as is.
+    {ok, Ip};
+parse_ip(Ip) when is_list(Ip) ->
+    %% A string that is parseable as IP is accepted.
+    inet:parse_address(Ip);
+parse_ip(Ip) when is_tuple(Ip) ->
+    %% IP address in tuple format is also accepted.
+    case inet:is_ipaddress(Ip) of
+        true -> {ok, Ip};
+        _Else -> {error, invalid_ip_address}
+    end.
--- server_SUITE.erl
+++ server_SUITE.erl
@@ -11,14 +11,16 @@ groups() ->
     [{grp1, [], Grp1}].
 
 tc_server_onetime_request(_Config) ->
+    Host = "localhost",
     Port = 7777,
     spawn_link(server, start, [Port]),
-    {tcp, _Socket, "Echo Hello"} = client:start(Port),
+    {tcp, _Socket, "Echo Hello"} = client:start(Host, Port),
     ok.
 
 tc_server_multiple_client_requests(_Config) ->
+    Host = "localhost",
     Port = 7777,
     spawn_link(server, start, [Port]),
-    {tcp, Socket, "Echo Hello"} = client:start(Port),
-    {tcp, Socket, "Echo Hello"} = client:start(Port),
+    {tcp, Socket, "Echo Hello"} = client:start(Host, Port),
+    {tcp, Socket, "Echo Hello"} = client:start(Host, Port),
     ok.
